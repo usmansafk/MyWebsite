ee12281b126a66f3c94a5fba84ad72f1c39d9cb2
Usman Sajid <usman230@hotmail.co.uk>
2021-07-22 16:14:29 +0100

    homepage structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import NavBar from './components/nav/NavBar';
-import Home from './components/home/Home.jsx';
+import './App.scss';
+import { useState} from "react";
 
+//Components
+import NavBar from './components/nav/NavBar';
+import Welcome from './components/home/welcome/Welcome';
+import AboutMe from './components/home/aboutme/Aboutme';
+import TechStack from './components/home/techstack/Techstack';
+import Portfolio from './components/home/portfolio/Portfolio';
+import Contact from './components/home/contact/Contact'
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
     <div className='app'>
-      <NavBar />
-
-
-      
-      <Home />
-      
-
+      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className='sections'>
+        <Welcome />
+        <AboutMe />
+        <TechStack />
+        <Portfolio />
+        <Contact />
+      </div>
     </div>
   );
 }
