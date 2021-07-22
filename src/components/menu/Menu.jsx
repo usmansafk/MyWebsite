530b12437ab833a2593cadd9a93f94fade5f579c
Usman Sajid <usman230@hotmail.co.uk>
2021-07-22 16:19:13 +0100

    burger menu

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./MenuStyle.scss";
+
+const Menu = () => {
+    return (
+        <div className="menu">
+        <ul>
+            <li>
+            
+            
+            </li>
+        </ul>
+
+
+            
+        </div>
+    )
+}
+
+export default Menu
