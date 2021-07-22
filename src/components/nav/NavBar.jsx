dbc6e5dba6f72c2e308e42818052509a3438fe12
Usman Sajid <usman230@hotmail.co.uk>
2021-07-22 16:20:12 +0100

    nav bar tweaks

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './NavBarStyle.scss'
 
-const NavBar = () => {
+import { LinkedIn, GitHub, Email } from '@material-ui/icons';
+
+const NavBar = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className = 'navbar'>
-            
+        <div className={"navbar " + (menuOpen && "active")}>
+            <div className='wrapper'>
+
+                <div className="left">
+                    <a href="https://www.linkedin.com/in/usmansajidcode/" target="_blank">
+                        <LinkedIn className="icons" />
+                    </a>
+        
+                    <a href="https://github.com/usmansafk" target="_blank">
+                        <GitHub className="icons" />
+                    </a>
+
+                    <a href="mailto:usman230@hotmail.co.uk">
+                     <Email className="icons" />
+                     </a>
+                </div>
+
+                <div className="center">
+                    <a href="#welcome" className="logo">usmans~afk</a>
+                </div>
+
+
+                <div className="right">
+                    <div className="burger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+
+                </div>
+
+     
+            </div>
+
         </div>
     )
 }
