fad8ffdcf0526b40ea9f7c2b60b34fa4b1f52ff5
Usman Sajid <usman230@hotmail.co.uk>
2021-07-25 18:52:32 +0100

    misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './App.scss';
-import { useState} from "react";
+import "./App.scss";
+import { useState } from "react";
 
 //Components
-import NavBar from './components/nav/NavBar';
-import Welcome from './components/home/welcome/Welcome';
-import AboutMe from './components/home/aboutme/Aboutme';
-import TechStack from './components/home/techstack/Techstack';
-import Portfolio from './components/home/portfolio/Portfolio';
-import Contact from './components/home/contact/Contact'
+import NavBar from "./components/nav/NavBar";
+import Welcome from "./components/home/welcome/Welcome";
+import AboutMe from "./components/home/aboutme/Aboutme";
+import TechStack from "./components/home/techstack/Techstack";
+import Portfolio from "./components/home/portfolio/Portfolio";
+import Contact from "./components/home/contact/Contact";
+import Menu from "./components/menu/Menu";
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(true);
-
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className='app'>
-      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <div className='sections'>
+    <div className="app">
+      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="sections">
         <Welcome />
         <AboutMe />
         <TechStack />
