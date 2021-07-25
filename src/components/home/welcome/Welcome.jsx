df208778787e344c86f44368bf9330ef42e48881
Usman Sajid <usman230@hotmail.co.uk>
2021-07-25 18:41:34 +0100

    finshed hero section

--- src/components/home/welcome/Welcome.jsx
+++ src/components/home/welcome/Welcome.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import '../welcome/WelcomeStyle.scss'
+import React, { useEffect, useRef } from "react";
+import "../welcome/WelcomeStyle.scss";
+import { init } from "ityped";
 
 const Welcome = () => {
-    return (
-        <div className='welcome' id='welcome'>
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ["Software Engineer", "Web Developer", "Photographer"]
+    });
+  }, []);
+
+  return (
+    <div className="welcome" id="welcome">
+      <div className="left">
+        <div className="wrap">
+          <h2>Hello, I'm</h2>
+          <h1>Usman Sajid</h1>
+          <h3>
+            and I'm a <span ref={textRef}></span>
+          </h3>
+          <a href="#aboutme">
+            <img src="resources/down-arrow.svg" alt="down arrow" />
+          </a>
         </div>
-    )
-}
+      </div>
+
+      <div className="right">
+        <div className="imgContainer">
+          <img
+            className="me"
+            src="resources/UsmanSajid.png"
+            alt="Usman Sajid"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
