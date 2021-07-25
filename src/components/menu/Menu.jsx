e96ca36de6a4b9f710eff279ec637795de7216aa
Usman Sajid <usman230@hotmail.co.uk>
2021-07-25 18:43:37 +0100

    added collections tab to menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React from "react";
 import "./MenuStyle.scss";
 
-const Menu = () => {
-    return (
-        <div className="menu">
-        <ul>
-            <li>
-            
-            
-            </li>
-        </ul>
-
-
-            
-        </div>
-    )
+//=>
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu " + (menuOpen && "active")}>
+      <ul>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#welcome">Welcome</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#aboutme">About Me</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#techstack">Tech Stack</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>~</li>
+        <br />
+        <li>
+          <a href="#blog" className="blog">
+            Blog
+          </a>
+          <h6>Coming Soon</h6>
+        </li>
+        <li>
+          <a href="#collections" className="blog">
+            Collections
+          </a>
+          <h6>Coming Soon</h6>
+        </li>
+      </ul>
+    </div>
+  );
 }
-
-export default Menu
