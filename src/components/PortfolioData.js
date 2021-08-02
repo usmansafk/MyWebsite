e49bc52c86cddd3ef1286b8d76cf5bf9dfe80a65
Usman Sajid <usman230@hotmail.co.uk>
2021-08-02 18:19:46 +0100

    created js file with json methods for ease when adding future projects

---
+++ src/components/PortfolioData.js
@@ -0,0 +1,39 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Anime Watch List",
+        img:"resources/AWL-LOGO.png",
+        link: "https://github.com/usmansafk/Anime-Watch-List"
+    },
+    {
+        id: 2,
+        title: "QA Cinemas",
+        img:"resources/QA-CINE-LOGO.png",
+        link:"https://github.com/usmansafk/qa-cinemas"
+    },
+];
+
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Anime Watch List",
+        img:"resources/AWL-LOGO.png",
+        link: "https://github.com/usmansafk/Anime-Watch-List"
+    },
+    {
+        id: 2,
+        title: "QA Cinemas",
+        img:"resources/QA-CINE-LOGO.png",
+        link:"https://github.com/usmansafk/qa-cinemas"
+    },
+   
+
+];
+
+
+export const mobilePortfolio  = [];
+
+export const graphicsPortfolio = [];
+
+export const photographyPortfolio = [];
