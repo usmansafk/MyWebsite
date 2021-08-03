863cf5025b4082ae4ab6c4b2cb500767ab909724
Usman Sajid <usman230@hotmail.co.uk>
2021-08-03 23:33:44 +0100

    Project Slider DataFile

---
+++ src/components/PortfolioSliderData.js
@@ -0,0 +1,32 @@
+export const SliderData = [
+    {
+        id: 1,
+        logo: "./resources/web-site.png",
+        title: "AWL",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elt.Doloremque debitis veritatis animi tenetur, nemo labor!",
+        link: "https://github.com/usmansafk/Anime-Watch-List",
+        image: "./resources/AWL-2.jpg",
+        bgColour: "#131313"
+
+    },
+    {
+        id:2,
+        logo: "./resources/web-site.png",
+        title: "AWL II",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elt.Doloremque debitis veritatis animi tenetur, nemo labor!",
+        link: "https://github.com/usmansafk/qa-cinemas",
+        image: "./resources/AWL-1.png",
+        bgColour: "#131313"
+    },
+    {
+        id: 3,
+        logo: "./resources/web-site.png",
+        title: "QA Cinemas",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elt.Doloremque debitis veritatis animi tenetur, nemo labor!",
+        link: "https://github.com/usmansafk/qa-cinemas",
+        image: "./resources/QA-Cine1.png",
+        bgColour: "#131313"
+    },
+
+
+]
